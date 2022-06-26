4277f1d000e422a429e0c95566fb6841cabc41bf
Thien Nguyen <nguthien2002@gmail.com>
2022-06-26 15:50:18 -0600

    morse

---
+++ morse/index.js
@@ -0,0 +1,16 @@
+const morse = require("morse-code-converter");
+module.exports = async function (context, req) {
+  context.log("JavaScript HTTP trigger function processed a request.");
+  var plaintext = req.query.plaintext;
+  code = "";
+  if (typeof plaintext === "undefined" || plaintext === "") {
+    code = "Please enter some text to  convert!";
+  } else {
+    code = morse.textToMorse(plaintext);
+  }
+
+  context.res = {
+    // status: 200, /* Defaults to 200 */
+    body: code,
+  };
+};
