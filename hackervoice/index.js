edf09bb7316f3a3610dc57695e07b0057c455735
Thien Nguyen <nguthien2002@gmail.com>
2022-06-26 15:06:08 -0600

    hackervoice

--- hackervoice/index.js
+++ hackervoice/index.js
@@ -3,10 +3,15 @@ module.exports = async function (context, req) {
 
   const name = req.query.name || (req.body && req.body.name);
   var password = req.query.password;
-  context.log(password);
+  var resp = "";
+  if (password === "letmein") {
+    resp = "Access granted.";
+  } else {
+    resp = "Access denied.";
+  }
 
   context.res = {
     // status: 200, /* Defaults to 200 */
-    body: password,
+    body: resp,
   };
 };
