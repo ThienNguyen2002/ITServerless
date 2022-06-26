405e34b8cb28aa9d278a06fe7ca14a54c35bb394
Thien Nguyen <nguthien2002@gmail.com>
2022-06-26 17:00:31 -0600

    emotionalgifs

--- emotionalgifs/index.js
+++ emotionalgifs/index.js
@@ -11,9 +11,36 @@ module.exports = async function (context, req) {
   // parse the body
   const parts = multipart.Parse(body, boundary);
 
-  let convertedResult = Buffer.from(parts[0].data).toString("base64");
+  //module.exports function
+  //analyze the image
+  const result = await analyzeImage(parts[0].data);
   context.res = {
-    // status: 200, /* Defaults to 200 */
-    body: convertedResult,
+    body: {
+      result,
+    },
   };
+  console.log(result);
+  context.done();
 };
+
+async function analyzeImage(img) {
+  const subscriptionKey = process.env.SUBSCRIPTIONKEY;
+  const uriBase = process.env.ENDPOINT + "/face/v1.0/detect";
+
+  let params = new URLSearchParams({
+    returnFaceId: "true",
+    returnFaceAttributes: "emotion", //FILL IN THIS LINE
+  });
+
+  //COMPLETE THE CODE
+  let resp = await fetch(uriBase + "?" + params.toString(), {
+    method: "POST", //WHAT TYPE OF REQUEST?
+    body: img, //WHAT ARE WE SENDING TO THE API?
+    headers: {
+      "Content-Type": "application/octet-stream", //do this in the next section
+    },
+  });
+  let data = await resp.json();
+
+  return data;
+}
