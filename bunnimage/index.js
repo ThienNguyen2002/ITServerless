197fb7724a3f7ddc70275a2c730b2dd27a8ffd43
Thien Nguyen <nguthien2002@gmail.com>
2022-07-13 15:33:35 -0600

    bunnimage frontend

--- bunnimage/index.js
+++ bunnimage/index.js
@@ -1,10 +1,8 @@
-const bunnForm = document.getElementById("bunnForm");
-bunnForm.addEventListener("submit", function (event) {
-  event.preventDefault();
-  const username = document.getElementById("username").value;
-  if (username === "") {
-    alert("No name error.");
-  }
-  const output = document.getElementById("output");
-  output.textContent = "Thanks!";
+const bunnForm = document.getElementById('bunnForm');
+
+bunnForm.addEventListener('submit', function (event) {
+   event.preventDefault()
+   const username = document.getElementById("username").value
+   const output = document.getElementById("output")
+   output.textContent = username + "❤"
 });
